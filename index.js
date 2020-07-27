34ccc4dd7396e70b92377969414273a1ce4d857d
Peter Sirka <petersirka@gmail.com>
2020-07-27 20:55:25 +0200

    Renamed `mm` to `wm` as `websocket_message`.

--- index.js
+++ index.js
@@ -16241,7 +16241,7 @@ function runsnapshot() {
 		stats.memory = (memory.heapUsed / 1024 / 1024).floor(2);
 		stats.rm = F.stats.performance.request.floor(2);  // request min
 		stats.fm = F.stats.performance.file.floor(2);     // files min
-		stats.mm = F.stats.performance.message;
+		stats.wm = F.stats.performance.message;
 		stats.usage = F.stats.performance.usage.floor(2); // app usage in %
 		stats.requests = F.stats.request.request;
 		stats.pending = F.stats.request.pending;
