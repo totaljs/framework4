00330490eac6ed34723262853ed677d9f8f0bbd3
Peter Sirka <petersirka@gmail.com>
2020-12-05 17:12:06 +0100

    Fixed options.

--- helpers/index.js
+++ helpers/index.js
@@ -16,12 +16,12 @@ const options = {};
 // options.livereload = 'https://yourhostname';
 
 // Enables cluster:
-// opt.cluster = 'auto';
+// options.cluster = 'auto';
 
 // Enables threads:
-// opt.cluster = 'auto';
-// opt.threads = '/api/';
-// opt.logs = 'isolated';
+// options.cluster = 'auto';
+// options.threads = '/api/';
+// options.logs = 'isolated';
 
 var type = process.argv.indexOf('--release', 1) !== -1 || process.argv.indexOf('release', 1) !== -1 ? 'release' : 'debug';
 require(total + '/' + type)(options);
