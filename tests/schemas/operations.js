bc04aa89ec6f1fa7f555b71bb840d4f79606d788
Jakub Urban <urbanjakub1306@gmail.com>
2021-02-01 14:59:09 +0100

    Basic operation tests

---
+++ tests/schemas/operations.js
@@ -0,0 +1,25 @@
+NEWSCHEMA('Operations', function(schema) {
+
+	schema.define('value', 'Object');
+
+	schema.addWorkflow('success', function($) {
+		OPERATION('OperationSuccess', $.callback);
+	});
+
+	schema.addWorkflow('invalid', function($) {
+		OPERATION('OperationInvalid', $.callback);
+	});
+
+	schema.addWorkflow('value', function($, model) {
+		OPERATION('OperationValue', model, $.callback);
+	});
+
+	schema.addWorkflow('run_invalid', function($, model) {
+		RUN('OperationSuccess, OperationInvalid, OperationValue', model, $.callback);
+	}, 'OperationValue');
+
+	schema.addWorkflow('run_success', function($, model) {
+		RUN('OperationSuccess, OperationValue', model, $.callback);
+	});
+
+});
