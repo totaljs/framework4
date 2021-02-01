d9b122f752bf70fe4e8b12dbf4ff54e8dc537a86
Peter Širka <petersirka@gmail.com>
2021-02-01 16:31:36 +0100

    Improved code.

--- tests/controllers/operation.js
+++ tests/controllers/operation.js
@@ -1,15 +1,15 @@
 exports.install = function() {
 
-	ROUTE('GET   /operations/success/                    *           --> OperationSuccess');
-	ROUTE('GET   /operations/invalid/                    *           --> OperationInvalid');
-	ROUTE('POST  /operations/value/                      *           --> OperationValue');
+	ROUTE('GET     /operations/success/               *             --> OperationSuccess');
+	ROUTE('GET     /operations/invalid/               *             --> OperationInvalid');
+	ROUTE('POST    /operations/value/                 *             --> OperationValue');
 
-	ROUTE('GET   /operations/schema/success/             *Operations --> success');
-	ROUTE('GET   /operations/schema/invalid/             *Operations --> invalid');
-	ROUTE('POST  /operations/schema/value/               *Operations --> value');
+	ROUTE('GET     /operations/schema/success/        *Operations   --> success');
+	ROUTE('GET     /operations/schema/invalid/        *Operations   --> invalid');
+	ROUTE('POST    /operations/schema/value/          *Operations   --> value');
 
-	ROUTE('POST  /operations/schema/run/invalid/         *Operations --> run_invalid');
-	ROUTE('POST  /operations/schema/run/success/         *Operations --> run_success');
-	ROUTE('POST  /operations/schema/run/stop/            *Operations --> run_stop');
+	ROUTE('POST    /operations/schema/run/invalid/    *Operations   --> run_invalid');
+	ROUTE('POST    /operations/schema/run/success/    *Operations   --> run_success');
+	ROUTE('POST    /operations/schema/run/stop/       *Operations   --> run_stop');
 
-}
+};
