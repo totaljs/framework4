16d48f34d5611110e52e71f7588835bc399d9fed
Peter Širka <petersirka@gmail.com>
2021-03-10 16:03:21 +0100

    Uncomplete request processing.

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,7 @@
 require('../index.js');
 
-RESTBuilder.GET('http://127.0.0.1:8000').callback(console.log);
+// RESTBuilder.POST('https://docanym.totalavengers.com/download/', { url: 'https://www.totaljs.com/download/ajqm001ft41d.svg', keywords: ['logo'] }).callback(console.log);
+
+DOWNLOAD('https://www.uvo.gov.sk/vyhladavanie-dokumentov/document/3132897/content/1141691/download', 'aa.pdf', function(err) {
+	console.log(err);
+});
