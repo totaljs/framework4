12cfd1688964b2088cb13b124fe97754f79e329a
Peter Širka <petersirka@gmail.com>
2021-03-22 08:39:13 +0100

    Updated version header.

--- index.js
+++ index.js
@@ -1635,7 +1635,7 @@ function Framework() {
 
 	self.$id = null; // F.id ==> property
 	self.is4 = self.version = 4039;
-	self.version_header = '4.0.0';
+	self.version_header = '4';
 	self.version_node = process.version + '';
 	self.syshash = (__dirname + '-' + Os.hostname() + '-' + Os.platform() + '-' + Os.arch() + '-' + Os.release() + '-' + Os.tmpdir() + JSON.stringify(process.versions)).md5();
 	self.pref = global.PREF;
