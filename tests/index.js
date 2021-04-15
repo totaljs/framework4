fd015b31eea3a5c209f93160cb0d3a44c6200c03
Jakub Urban <urbanjakub1306@gmail.com>
2021-04-15 19:46:05 +0200

    cleanup

--- tests/index.js
+++
@@ -1,7 +0,0 @@
-require('../index.js');
-
-// RESTBuilder.POST('https://docanym.totalavengers.com/download/', { url: 'https://www.totaljs.com/download/ajqm001ft41d.svg', keywords: ['logo'] }).callback(console.log);
-
-DOWNLOAD('https://www.uvo.gov.sk/vyhladavanie-dokumentov/document/3132897/content/1141691/download', 'aa.pdf', function(err) {
-	console.log(err);
-});
