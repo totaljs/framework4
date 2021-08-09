aa3126c6f3b4667f38ca3e82ae209635b7c509af
Peter Širka <petersirka@gmail.com>
2021-08-09 15:12:04 +0200

    Fixed extracting data.

--- index.js
+++ index.js
@@ -6920,7 +6920,7 @@ global.RESTORE = function(filename, target, callback, filter) {
 
 		index++;
 		item = data.slice(0, index - 1).toString(ENCODING).trim();
-		data = data.slice(index);
+		data = data.slice(index + (data[index] === 32 ? 1 : 0));
 		type = 1;
 		parser.next();
 	};
