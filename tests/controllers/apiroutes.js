87d9d43590a61114d9bafd034aec5af2b31f356c
Peter Širka <petersirka@gmail.com>
2021-09-22 15:04:26 +0200

    Added missing semicolon.

--- tests/controllers/apiroutes.js
+++ tests/controllers/apiroutes.js
@@ -7,4 +7,4 @@ exports.install = function() {
 	ROUTE('API    /v1/    #api_keys            *APIRoutes   --> keys');
 	ROUTE('API    /v1/    #api_keys_multi      *APIRoutes   --> one two keys (response)');
 
-}
+};
