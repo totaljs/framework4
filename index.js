97f7df5917dcb5b311b57163dd08c5f25b959d03
Peter Širka <petersirka@gmail.com>
2021-12-01 11:59:46 +0100

    Updated version.

--- index.js
+++ index.js
@@ -1991,7 +1991,7 @@ function Framework() {
 	var self = this;
 
 	self.$id = null; // F.id ==> property
-	self.is4 = self.version = 4054;
+	self.is4 = self.version = 4055;
 	self.version_header = '4';
 	self.version_node = process.version + '';
 	self.syshash = (__dirname + '-' + Os.hostname() + '-' + Os.platform() + '-' + Os.arch() + '-' + Os.release() + '-' + Os.tmpdir() + JSON.stringify(process.versions)).md5();
