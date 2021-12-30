a96b996b9ffd4dbb3924c7eddcafe124831b1eab
Peter <peter@kineti.nl>
2021-12-30 18:01:36 +0100

    Verify BitExtractor arguments, use BigInt only when needed

--- bitextractor.js
+++ bitextractor.js
@@ -6,10 +6,15 @@ class BitExtractor {
 
 	constructor(data, from, to) {
 
-		var bin = this.toRadix(data, from).toString(2);
+		if (from < 2 || from > 36 || to < 2 || to > 36) {
+            throw new RangeError("Base must be must be between 2 and 36");
+        }
+
+		this.data = this._verifyData(data, from);
+
+		var bin = this._convertFromRadixToBin(data, from);
 
 		this.offset = 0;
-		this.data = data;
 		this.to = to;
 
 		this._bytes = bin.padStart(Math.ceil(bin.length / 8) * 8, '0');
@@ -20,6 +25,55 @@ class BitExtractor {
 			this._bits += this._bytes.substring(x * 8, (x * 8) + 8).split('').reverse().join('');
 
 	};
+
+	_verifyData(data, base) {
+        const alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+        
+		if (!data) 
+            throw new Error("No data");
+                
+		data = data.toLowerCase();
+        
+		var alphabetValues = {}
+        for (let i = 0; i < alphabet.length; i++) 
+            alphabetValues[alphabet[i]] = i+1;
+        
+		for (let c of data) 
+            if (!(c in alphabetValues && alphabetValues[c] <= base)) 
+                throw new RangeError(`${c} is not a valid digit in base ${base}`);
+        
+		return data;
+    }
+
+    _convertFromRadixToBin(value, radix) {
+        var val = 0;
+
+        if (radix === 2) 
+            return value;
+        else if (value.length >= Number.MAX_SAFE_INTEGER.toString(radix).length) {
+            if (radix == 16) {
+                if (value.substring(0, 2) !== '0x') {
+                    value = '0x' + value;
+                }
+                val = BigInt(value);
+            } else if (radix == 8) {
+                if (value.substring(0, 2) !== '0o') {
+                    value = '0o' + value;
+                }
+                val = BigInt(value);
+            } else {
+                var chunk = 10,
+                    factor = BigInt(radix ** chunk),
+                    i = value.length % chunk || chunk,
+                    parts = [value.slice(0, i)];
+                while (i < value.length) parts.push(value.slice(i, i += chunk));
+                val = parts.reduce((r, v) => r * factor + BigInt(parseInt(v, radix)), 0n);
+            }
+        } else 
+            val = parseInt(value, radix);        
+        
+        return val.toString(2);
+    }
 
 	toRadix(value, radix) {
 		var size = 10;
