abe17a76cccebc37cd89dcc4cbc7e1564dd87652
Peter Širka <petersirka@gmail.com>
2022-04-01 11:14:04 +0200

    Created new version `0.0.61`.

--- index.js
+++ index.js
@@ -2020,7 +2020,7 @@ function Framework() {
 	var self = this;
 
 	self.$id = null; // F.id ==> property
-	self.is4 = self.version = 4060;
+	self.is4 = self.version = 4061;
 	self.version_header = '4';
 	self.version_node = process.version + '';
 	self.syshash = (__dirname + '-' + Os.hostname() + '-' + Os.platform() + '-' + Os.arch() + '-' + Os.release() + '-' + Os.tmpdir() + JSON.stringify(process.versions)).md5();
