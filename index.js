41d0a610b92473ae48d432842bc14cf51ca77288
Peter Širka <petersirka@gmail.com>
2023-06-15 14:06:05 +0200

    Added new version.

--- index.js
+++ index.js
@@ -2175,7 +2175,7 @@ function Framework() {
 	var self = this;
 
 	self.$id = null; // F.id ==> property
-	self.is4 = self.version = 4086;
+	self.is4 = self.version = 4087;
 	self.version_header = '4';
 	self.version_node = process.version + '';
 	self.syshash = (__dirname + '-' + Os.hostname() + '-' + Os.platform() + '-' + Os.arch() + '-' + Os.release() + '-' + Os.tmpdir() + JSON.stringify(process.versions)).md5();
