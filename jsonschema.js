cdb58552b562a7422d02fd4b4111fef9d7a3528f
petersirka <petersirka@gmail.com>
2024-05-06 09:21:44 +0200

    Extended icon validator by adding support for `tic` keyword.

--- jsonschema.js
+++ jsonschema.js
@@ -2,7 +2,7 @@ require('./index');
 
 const REG_NUMBER = /[\d,.]+/;
 const REG_COLOR = /^#([A-F0-9]{3}|[A-F0-9]{6}|[A-F0-9]{8})$/i;
-const REG_ICON = /^(ti|far|fab|fad|fal|fas|fa)?\s(fa|ti)-[a-z0-9-]+$/;
+const REG_ICON = /^(ti|tic|far|fab|fad|fal|fas|fa)?\s(fa|ti)-[a-z0-9-]+$/;
 
 function check_string(meta, error, value, errplus, path) {
 
